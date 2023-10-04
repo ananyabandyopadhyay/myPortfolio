52a9eff84ee6be84a46d0c14cc0ae2dfc9d9f6ad
Ananya Bandyopadhyay <ani99banerjee@gmail.com>
2023-10-04 11:30:15 +0530

    1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,44 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'sans-serif'],
+      'ubuntu': ['Pixelify Sans', 'cursive'],
+      'courier': ['courier', 'ubuntu', 'sans-serif'],
+      'roboto-mono': ['roboto-mono', 'ubuntu', 'sans-serif'],
+      'pacifico': ['pacifico','ubuntu', 'sans-serif'],
+      'cookie': ['cookie','ubuntu', 'sans-serif'],
+      'Shadows Into Light': ['shadows-into-light','ubuntu', 'sans-serif'],
+      'dancing-script': ['Dancing Script'],
+      'josefin-sans': ['Josefin Sans', 'sans-serif']
+    },
+  
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'my-image': 'url("/portfolioImage1.jpeg")'
       },
+      colors: {
+        'primary-blue': '#264858',
+        'primary-yellow': '#7b4b1c'
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        'large': '8rem'
+      },
+      animation: {
+        fadeIn1s: 'fadeIn infinite',
+        fadeOut1s: 'fadeOut 1s infinite',
+        animation:  '5s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
